7b59e869c9b89e846f9469280f4fc041ce40a386
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-10-31 13:29:19 -0400

    created routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  
 
-  resources :users
-  
-
-
+  resources :coffees
   resources :reviews
+  
+  resources :users
+  post "/signin", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "signout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
   
 end

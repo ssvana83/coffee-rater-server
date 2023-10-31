c78443ad779c2e67c959da74fcd9766d5d3ad7e4
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-10-31 13:24:25 -0400

    application controller edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  before_action :authorized!
+
+
+  private
+  def authorized!
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+
 end

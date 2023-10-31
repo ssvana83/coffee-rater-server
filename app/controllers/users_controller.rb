9002e1acab1a3350498293c5da327f6032725f4e
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-10-31 13:38:17 -0400

    created actions in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized!, only: [:create]
+
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+
+  def show
+    render json: @current_user, status: :ok
+  end
+
+  
+  private
+  def user_params
+    params.permit(:username, :email, :password_digest)
+  end
+
 end

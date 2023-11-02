81bb15ac55b8c583d51a6a8c7d8e6a55b07b4426
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-11-02 11:36:02 -0400

    got signout working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user, status: :created
+    user = User.create(user_params)                         # this is the moment the user is created
+    if user.valid?
+      session[:user_id] = user.id                           # this is where the session is added to the user
+      render json: user, status: :created                   # which is the point that the user signs in
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
 
@@ -18,14 +22,14 @@ class UsersController < ApplicationController
     if user
       render json: user
     else
-      render json: {error: "Not Authorized"}, status: :unauthorized
+      render json: { error: "Not Authorized" }, status: :unauthorized
     end
   end
 
 
   private
   def user_params
-    params.permit(:username, :email, :password_digest)
+    params.permit(:username, :email, :password, :password_confirmation, :password_digest)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :coffees, through: :reviews
 
   has_secure_password
+  validates :username, :email, :password, :password_confirmation, presence: true
 end

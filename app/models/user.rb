da7335169b48f32e7ad3ed30c80532462712bd4f
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-11-07 20:13:43 -0500

    working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :username, :email, :password, :password_confirmation, presence: true
+
+  # validation here to prevent user form reviewing the same coffee twice 
 end
